583465c3a6b4bc78ef32992db3dc2621babccb1b
abialemuel <abia.darma@bukalapak.com>
2020-04-19 22:42:01 +0700

    change farmer_code to farmer_id

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+
         DB::table('sales')->insert([
             'warehouse_code' => Str::random(10),
             'needle_code' => Str::random(10),
@@ -23,9 +24,21 @@ class DatabaseSeeder extends Seeder
             'updated_by' => 1,
         ]);
 
+        DB::table('farmers')->insert([
+            'farmer_code' => Str::random(10),
+            'farmer_code' => Str::random(10),
+            'name' => Str::random(10),
+            'area' => Str::random(10),
+            'address' => Str::random(10),
+            'created_by' => 1,
+            'updated_by' => 1,
+        ]);
+
+        $farmer = DB::table('farmers')->first();
+
         DB::table('purchases')->insert([
             'ac_code' => Str::random(10),
-            'farmer_code' => Str::random(10),
+            'farmer_id' => $farmer->id,
             'tiam' => Str::random(10),
             'bruto' => 10000,
             'netto' => 10000,
