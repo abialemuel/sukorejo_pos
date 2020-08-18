3a54201b5ca03c229fecc53297cee34ff6a060d1
abialemuel <abia.darma@bukalapak.com>
2020-08-18 23:46:12 +0700

    add migration

--- database/migrations/2020_08_18_163113_create_payment_logs_table.php
+++ database/migrations/2020_08_18_163113_create_payment_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePurchaseOrdersTable extends Migration
+class CreatePaymentLogsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,12 @@ class CreatePurchaseOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('purchase_orders', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->foreignId('farmer_id');
-            $table->timestamp("purchased_at");
+        Schema::create('payment_logs', function (Blueprint $table) {
+            $table->id();
             $table->double("amount");
-            $table->softDeletes();
+            $table->string("paymentable_type");
+            $table->integer("paymentable_id");
             $table->timestamps();
-            $table->integer('created_by');	
-            $table->integer('updated_by');
         });
     }
 
@@ -32,6 +29,6 @@ class CreatePurchaseOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('purchase_orders');
+        Schema::dropIfExists('payment_logs');
     }
 }
