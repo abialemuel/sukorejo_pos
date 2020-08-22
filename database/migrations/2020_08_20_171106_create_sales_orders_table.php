523be856939cc745e97704c8d772c99abb5e6a62
abialemuel <abia.darma@bukalapak.com>
2020-08-22 20:55:41 +0700

    sales_orders migration

--- database/migrations/2020_08_20_171106_create_sales_orders_table.php
+++ database/migrations/2020_08_20_171106_create_sales_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSalesTable extends Migration
+class CreateSalesOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateSalesTable extends Migration
      */
     public function up()
     {
-        Schema::create('sales', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('tiam');
-            $table->double('bruto');
-            $table->double('netto');
-            $table->double('price');
+        Schema::create('sales_orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('warehouse_code');
+            $table->string('needle_code');
+            $table->timestamp("sold_at");
+            $table->double("amount");
             $table->softDeletes();
             $table->timestamps();
             $table->integer('created_by');	
@@ -33,6 +33,6 @@ class CreateSalesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sales');
+        Schema::dropIfExists('sales_orders');
     }
 }
