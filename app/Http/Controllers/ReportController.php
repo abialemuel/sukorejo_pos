1b31e46c7826c20ff9431038c2051c238dff8395
Krian Bayu <krian@strongbee.co.id>
2020-09-03 01:24:08 +0700

    found bug report pnl

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -30,18 +30,18 @@ class ReportController extends Controller
     public function printPdf($tanggal)
     {
         //
-        $reports = DB::table('sales AS s')
-            ->join('purchases AS p', function ($join) {
+        $reports = DB::table('sale_orders AS s')
+            ->join('purchase_orders AS p', function ($join) {
                 $join->on(DB::raw("DATE_FORMAT(s.created_at , '%Y-%m-%d')"), "=", DB::raw("DATE_FORMAT(p.created_at , '%Y-%m-%d')"));
             })
             ->select(DB::raw("DATE_FORMAT(s.created_at , '%Y-%m-%d') AS created_at"), 
                              's.tiam', 's.bruto', 
                              's.netto' , 's.warehouse_code', 
                              's.needle_code', 
-                             DB::raw('SUM(s.price) as total_sales'), 
-                             DB::raw('SUM(p.price) as total_purchases'), 
-                             (DB::raw('SUM(s.price)-SUM(p.price) as total_laba_rugi')))
-            ->groupBy(DB::raw("DATE_FORMAT(s.created_at , '%Y-%m-%d')"), 's.tiam', 's.bruto', 's.netto', 's.warehouse_code', 's.needle_code')
+                             'SUM(s.amount) as total_sales', 
+                             'SUM(p.amount) as total_purchases', 
+                             'SUM(s.amount)-SUM(p.amount) as total_laba_rugi')
+            ->groupBy(DB::raw("DATE_FORMAT(s.created_at , '%Y-%m-%d')"))
             ->get();
     	$pdf = PDF::loadview('pages.pdf.pnl_report', compact('reports'));
     	return $pdf->stream();
